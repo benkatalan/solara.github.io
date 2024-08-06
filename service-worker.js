f42de46a1ef1d992e719828017fc7df301a69fe3
benkatalan <98488242+benkatalan@users.noreply.github.com>
2024-08-06 22:09:37 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('surfly-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/app.html',
+                '/about.html',
+                '/styles.css',
+                '/script.js',
+                '/beaches.json',
+                '/images/hero1.jpg',
+                '/images/hero2.jpg',
+                '/images/hero3.jpg',
+                '/images/surfer.jpg',
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
